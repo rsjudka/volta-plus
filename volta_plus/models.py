996cdebf160e918e0930e08b113aff5857db134b
Robert Judka <robertjudka@gmail.com>
2020-02-03 22:42:23 -0600

    adding site abstraction; plan on checking currently stored data against what was parsed

--- volta_plus/models.py
+++ volta_plus/models.py
@@ -1,14 +1,20 @@
 from collections import namedtuple
 from datetime import datetime
-from google.api_core.datetime_helpers import DatetimeWithNanoseconds
-from google.cloud import firestore
 import json
 import logging
 import os.path
-import pytz
-from timezonefinder import TimezoneFinder
 from urllib.request import urlopen
 
+from google.api_core.datetime_helpers import DatetimeWithNanoseconds
+from google.cloud import firestore
+import pytz
+from timezonefinder import TimezoneFinder
+
+
+_db = firestore.Client()
+sites_ref = _db.collection('sites')
+stations_ref = _db.collection('stations')
+meters_ref = _db.collection('meters')
 
 def log_warning(msg, data):
     logging.warning("--------------------------------------------------------------")
@@ -171,7 +177,7 @@ class VoltaStation:
         volta_station.stale = False
 
         return volta_station
-    
+
     def update(self, new_name, new_status, new_street_address, new_city, new_state, new_zip_code, new_timezone):
         if new_name != self.name:
             self.name = new_name
@@ -195,7 +201,7 @@ class VoltaStation:
             self.timezone = new_timezone
             self.stale =  True
 
-    def serialize(self, meters_ref):
+    def serialize(self):
         return {
             'name': self.name,
             'status': self.status,
@@ -207,30 +213,121 @@ class VoltaStation:
             'meters': [meters_ref.document(meter_id) for meter_id in self.meters]
         }
 
+class VoltaSite:
+    def __init__(self, name, street_address, city, state, zip_code, timezone):
+        self.name = name
+        self.street_address = street_address
+        self.city = city
+        self.state = state
+        self.zip_code = zip_code
+        self.timezone = timezone
+
+        self.stations = dict()
+
+        self.stale = True
+
+    @classmethod
+    def from_collection(cls, collection):
+        name = collection['name']
+        street_address = collection['street_address']
+        city = collection['city']
+        state = collection['state']
+        zip_code = collection['zip_code']
+        zone = collection['timezone']
+        timezone = pytz.timezone(zone) if zone is not None else None
+
+        volta_site = cls(name, street_address, city, state, zip_code, timezone)
+        volta_site.stale = False
+
+        return volta_site
+
+    def update(self, new_name, new_street_address, new_city, new_state, new_zip_code, new_timezone):
+        if new_name != self.name:
+            self.name = new_name
+            self.stale =  True
+        if new_street_address != self.street_address:
+            self.street_address = new_street_address
+            self.stale =  True
+        if new_city != self.city:
+            self.city = new_city
+            self.stale =  True
+        if new_state != self.state:
+            self.state = new_state
+            self.stale =  True
+        if new_zip_code != self.zip_code:
+            self.zip_code = new_zip_code
+            self.stale =  True
+        if new_timezone != self.timezone:
+            self.timezone = new_timezone
+            self.stale =  True
+
+    def serialize(self):
+        return {
+            'name': self.name,
+            'street_address': self.street_address,
+            'city': self.city,
+            'state': self.state,
+            'zip_code': self.zip_code,
+            'timezone': self.timezone.zone,
+            'stations': [stations_ref.document(station_id) for station_id in self.stations]
+        }
+
 class VoltaNetwork:
     API_URL = 'https://api.voltaapi.com/v1/public-sites'
 
     def __init__(self):
-        self.stations = dict()
+        self.sites = dict()
 
         self.tf = TimezoneFinder(in_memory=True)
-
-        db = firestore.Client()
-        self.stations_ref = db.collection('stations')
-        self.meters_ref = db.collection('meters')
 
     def update(self):
         with urlopen(self.API_URL) as url:
             data = json.loads(url.read().decode())
-            for charger in data:
-                stations = charger.get('stations', None)
-                if stations is not None:
-                    for station in stations:
-                        self.parse_station(station)
-                else:
-                    log_warning("'stations' array not found", charger)
+            for site in data:
+                self.parse_site(site)
+            # compare {site.id for site in sites_ref.stream()} to set(self.sites.keys())
 
-    def parse_station(self, station):
+    def parse_site(self, site):
+        site_id = site.get('id', None)
+        if site_id is None:
+            log_warning("site 'id' not found", site)
+            return
+
+        name = site.get('name', None)
+        street_address = site.get('street_address', None)
+        city = site.get('city', None)
+        state = site.get('state', None)
+        zip_code = int(site['zip_code']) if 'zip_code' in site else None
+        timezone = self.find_timezone(site)
+
+        volta_site = self.sites.get(site_id, None)
+        station_refs = None
+        if volta_site is None:
+            site_attributes = ['name', 'street_address', 'city', 'state', 'zip_code', 'timezone', 'stations']
+            collection = sites_ref.document(site_id).get(site_attributes).to_dict()
+            station_refs = {station.get().id for station in collection['stations']}
+            if collection is not None:
+                volta_site = VoltaSite.from_collection(collection)
+            else:
+                volta_site = VoltaSite(name, street_address, city, state, zip_code, timezone)
+
+            self.sites[site_id] = volta_site
+
+        volta_site.update(name, street_address, city, state, zip_code, timezone)
+
+        stations = site.get('stations', None)
+        if stations is not None:
+            for station in stations:
+                self.parse_station(volta_site, station)
+            # compare station_refs to set(volta_site.stations.keys())
+        else:
+            log_warning("'stations' array not found", site)
+
+        if volta_site.stale:
+            sites_ref.document(site_id).set(volta_site.serialize())
+            volta_site.stale = False
+
+    def parse_station(self, volta_site, station):
         station_id = station.get('id', None)
         if station_id is None:
             log_warning("station 'id' not found", station)
@@ -238,21 +335,24 @@ class VoltaNetwork:
 
         name = station.get('name', None)
         status = station.get('status', None)
-        timezone = self.find_timezone(station)
         street_address = station.get('street_address', None)
         city = station.get('city', None)
         state = station.get('state', None)
         zip_code = int(station['zip_code']) if 'zip_code' in station else None
-        
-        volta_station = self.stations.get(station_id, None)
+        timezone = self.find_timezone(station)
+
+        volta_station = volta_site.stations.get(station_id, None)
+        meter_refs = None
         if volta_station is None:
-            collection = self.stations_ref.document(station_id).get().to_dict()
+            station_attributes = ['name', 'status', 'street_address', 'city', 'state', 'zip_code', 'timezone', 'meters']
+            collection = stations_ref.document(station_id).get(station_attributes).to_dict()
+            meter_refs = collection['meters']
             if collection is not None:
                 volta_station = VoltaStation.from_collection(collection)
             else:
                 volta_station = VoltaStation(name, status, street_address, city, state, zip_code, timezone)
 
-            self.stations[station_id] = volta_station
+            volta_site.stations[station_id] = volta_station
 
         volta_station.update(name, status, street_address, city, state, zip_code, timezone)
 
@@ -260,12 +360,38 @@ class VoltaNetwork:
         if meters is not None:
             for meter in meters:
                 self.parse_meter(volta_station, meter)
+            # compare meter_refs to set(volta_station.meters.keys())
         else:
             log_warning("'meters' array not found", station)
 
         if volta_station.stale:
-            self.stations_ref.document(station_id).set(volta_station.serialize(self.meters_ref))
+            stations_ref.document(station_id).set(volta_station.serialize())
             volta_station.stale = False
+
+    def parse_meter(self, volta_station, meter):
+        meter_id = meter.get('oem_id', None)
+        if meter_id is None:
+            log_warning("meter 'oem_id' not found", meter)
+            return
+
+        state = meter['state'].lower() if 'state' in meter else None
+        availability = meter['availability'].lower() if 'availability' in meter else None
+
+        volta_meter = volta_station.meters.get(meter_id, None)
+        if volta_meter is None:
+            meter_attributes = ['weekly_usage', 'in_use_charging_stats', 'in_use_stopped_stats']
+            collection = meters_ref.document(meter_id).get(meter_attributes).to_dict()
+            if collection is not None:
+                volta_meter = VoltaMeter.from_collection(collection)
+            else:
+                volta_meter = VoltaMeter()
+
+            volta_station.meters[meter_id] = volta_meter
+
+        volta_meter.update(state, availability, volta_station.timezone)
+        if volta_meter.stale:
+            meters_ref.document(meter_id).set(volta_meter.serialize())
+            volta_meter.stale = False
 
     def find_timezone(self, station):
         location = station.get('location', None)
@@ -283,27 +409,3 @@ class VoltaNetwork:
             log_warning("'location' object not found", station)
 
         return None
-
-    def parse_meter(self, volta_station, meter):
-        meter_id = meter.get('oem_id', None)
-        if meter_id is None:
-            log_warning("meter 'oem_id' not found", meter)
-            return
-
-        state = meter['state'].lower() if 'state' in meter else None
-        availability = meter['availability'].lower() if 'availability' in meter else None
-
-        volta_meter = volta_station.meters.get(meter_id, None)
-        if volta_meter is None:
-            collection = self.meters_ref.document(meter_id).get().to_dict()
-            if collection is not None:
-                volta_meter = VoltaMeter.from_collection(collection)
-            else:
-                volta_meter = VoltaMeter()
-
-            volta_station.meters[meter_id] = volta_meter
-
-        volta_meter.update(state, availability, volta_station.timezone)
-        if volta_meter.stale:
-            self.meters_ref.document(meter_id).set(volta_meter.serialize())
-            volta_meter.stale = False
